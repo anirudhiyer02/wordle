cf7bcba0a0c143e43aa6be5cd36263d0d99b405c
Anirudh Iyer <anirudhiyer@ResNet-17-146.resnet.ucsb.edu>
2022-02-14 00:11:01 -0800

    more changes

--- wordle.py
+++ wordle.py
@@ -1,19 +1,13 @@
-reducedSet = []
-solutions = open('wordlist.txt','r')
-print(len(reducedSet))         
 reducedSet=[]
 solutions=open('wordlist.txt')
-for j in solutions:
-    j=j[3:8]
-    reducedSet.append(j)
-#reducedSet.remove('which')
-#print(reducedSet[0])
+for s in solutions:
+    s=s[3:8]
+    reducedSet.append(s)
 lettersContain=[]
 correctLetters={}
 correct=True
 solutions=open('wordlist.txt')
 word=input("enter solution\n")#write
-#guess=input("enter guess\n")#think
 for count in range(6):
     guess=input("enter guess\n")#think
     if(guess==word):
@@ -23,38 +17,23 @@ for count in range(6):
         if(word[i]==guess[i]):
             print(word[i], end="")
             correctLetters[i]=word[i]
-            #if(not lettersContain.__contains__(guess[i])):
-                #lettersContain.append(guess[i])
-            #reducedSet.append(i)
         elif(word.__contains__(guess[i]) and not lettersContain.__contains__(guess[i])):
             lettersContain.append(guess[i])
             print("_", end="")
         else: 
             print("_", end="")
-    #solutions=open('wordlist.txt') 
     print(lettersContain)
-    #t=0
     for l in range(len(reducedSet)):
-        #if(count==0):
-        #l = l[3:8]
-        for i in range(5):
+        for i in range(5):#filters correct lettersand indices
             if(i in correctLetters):
                 if(not reducedSet[l]==" " and not correctLetters[i] == reducedSet[l][i]):
                     reducedSet[l]=" "
                     break;
-            #elif(correctLetters.__len__==5):
-             #   print('youve won')
-              #  quit();
-        for i in lettersContain:
+        for i in lettersContain:#filters letters that solution contains
             if (not reducedSet[l]==" " and not reducedSet[l].__contains__(i)):
                 reducedSet[l]=" "
                 break;
-    #print(reducedSet)
-    #print(len(reducedSet))
-    #guess =input("enter guess")
 for sol in reducedSet:
     if(not sol==" "):
         print(sol)
         break;
-#print(guess)
-#print(len(lettersContain))
